3d686ec8e9b76635ab20986a9606640a54eb189b
HERON NOERNBERG NAVES <16548851@acd.puc-campinas.edu.br>
2017-03-24 20:24:54 -0300

    Primeiro Up, versão 0.0.1; Codinome Ultra Pré Alfa

---
+++ Assets/Scripts/MovimentacaoInimigo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentacaoInimigo : MonoBehaviour {
+
+	private GameObject[] players;
+	public float velocidade;
+
+	private float distancia1, distancia2;
+	private Rigidbody2D corpo;
+
+	// Use this for initialization
+	void Start () {
+		
+		Debug.Log (GameObject.FindGameObjectsWithTag ("Player"));
+		corpo = GetComponent<Rigidbody2D> ();
+
+		players = new GameObject[2];
+		players [0] = GameObject.Find("Player 1");
+		players [1] = GameObject.Find("Player 2");
+
+		velocidade = velocidade * Time.deltaTime;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+		distancia1 = Mathf.Sqrt (players [0].GetComponent<Transform> ().position.x * players [0].GetComponent<Transform> ().position.x + players [0].GetComponent<Transform> ().position.y * players [0].GetComponent<Transform> ().position.y );
+		distancia2 = Mathf.Sqrt (players [1].GetComponent<Transform> ().position.x * players [1].GetComponent<Transform> ().position.x + players [1].GetComponent<Transform> ().position.y * players [1].GetComponent<Transform> ().position.y );				
+
+		if (distancia1 < distancia2) {
+			if (players [0].GetComponent<Transform> ().position.x > transform.position.x)
+				corpo.velocity = new Vector2 (velocidade, corpo.velocity.y);
+			else
+				corpo.velocity = new Vector2 (-velocidade, corpo.velocity.y);
+
+		} else {
+			if (players [1].GetComponent<Transform> ().position.x > transform.position.x)
+				corpo.velocity = new Vector2 (velocidade, corpo.velocity.y);
+			else
+				corpo.velocity = new Vector2 (-velocidade, corpo.velocity.y);
+			
+
+
+		}
+	
+	}
+}
---
+++ Assets/Scripts/MovimentacaoPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentacaoPlayer : MonoBehaviour {
+
+	public KeyCode BotaoParaCima, BotaoParaBaixo, BotaoParaDireita, BotaoParaEsquerda;
+	public float velocidade = 300, forcaPulo = 300;
+
+	private Rigidbody2D corpo;
+	// Use this for initialization
+	void Start () {
+
+		corpo = GetComponent<Rigidbody2D> ();
+		Debug.Log (KeyCode.UpArrow);
+		Debug.Log (KeyCode.DownArrow);
+		Debug.Log (KeyCode.LeftArrow);
+		Debug.Log (KeyCode.RightArrow);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if (Input.GetKeyDown (BotaoParaBaixo))
+
+		if (Input.GetKey (BotaoParaDireita))
+			corpo.velocity = new Vector2 (velocidade * Time.deltaTime, corpo.velocity.y); 
+
+		if (Input.GetKey (BotaoParaEsquerda))
+			corpo.velocity = new Vector2 (-velocidade * Time.deltaTime, corpo.velocity.y); 
+
+		if (Input.GetKeyDown (BotaoParaCima))
+			corpo.AddForce (new Vector2 (0, forcaPulo * Time.deltaTime * 100));
+		
+
+			
+			
+		
+	}
+}
